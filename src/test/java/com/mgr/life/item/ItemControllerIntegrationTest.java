8f493961e4d1bad8f71e13b8f1d25a63b82506d9
Marcel Gimeno Roman <marcel.gimeno@gmaill.com>
2018-04-16 12:46:46 +0200

    :sparkles: Basic Item GET with dummy data, Unit Tests & Integration Tests

---
+++ src/test/java/com/mgr/life/item/ItemControllerIntegrationTest.java
@@ -0,0 +1,55 @@
+package com.mgr.life.item;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.List;
+
+import static com.mgr.life.item.ItemController.END_POINT;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ItemControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + END_POINT);
+    }
+
+    @Test
+    public void planGetTest() {
+        ResponseEntity<List<Item>> response = template.exchange(base.toString(), HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Item>>() {
+                });
+        assertThat(response.getBody(), notNullValue());
+
+        for (int i = 0; i < 2; i++) {
+            assertThat(response.getBody().get(i).getId(), equalTo((long) i));
+            assertThat(response.getBody().get(i).getName(), equalTo("Item " + i));
+            assertThat(response.getBody().get(i).getType(), equalTo("Type " + i));
+            assertThat(response.getBody().get(i).getPrice(), equalTo(new BigDecimal(1000 * (i + 1))));
+        }
+    }
+}
---
+++ src/test/java/com/mgr/life/item/ItemControllerTest.java
@@ -0,0 +1,45 @@
+package com.mgr.life.item;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.mgr.life.item.ItemController.END_POINT;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void itemGetTest() throws Exception {
+        ResultActions resultActions = mvc.perform(
+                MockMvcRequestBuilders
+                        .get(END_POINT)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(2)));
+
+        for (int i = 0; i < 2; i++) {
+            resultActions
+                    .andExpect(jsonPath("$[" + i + "].id", is(i)))
+                    .andExpect(jsonPath("$[" + i + "].name", is("Item " + i)))
+                    .andExpect(jsonPath("$[" + i + "].type", is("Type " + i)))
+                    .andExpect(jsonPath("$[" + i + "].price", is(1000 * (i + 1))));
+        }
+    }
+}
