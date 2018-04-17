ea72b5458ac8cb5619073255f2a54c64315e7693
Marcel Gimeno Roman <marcel.gimeno@gmaill.com>
2018-04-17 11:23:20 +0200

    :sparkles: Added basic DELETE Item API operation.

--- src/main/java/com/mgr/life/item/ItemController.java
+++ src/main/java/com/mgr/life/item/ItemController.java
@@ -3,14 +3,15 @@ package com.mgr.life.item;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.mgr.life.item.ItemController.END_POINT;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController()
 @RequestMapping(path = END_POINT)
@@ -22,21 +23,28 @@ public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = GET)
     @ApiOperation(value = "Find all Items", response = Item.class, responseContainer = "List")
     public Iterable<Item> itemGet() {
         return itemRepository.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = POST)
     @ApiOperation(value = "Save or update an Item", response = Item.class)
     public ResponseEntity<Item> itemPost(@RequestBody Item item) {
-        return new ResponseEntity<>(itemRepository.save(item), HttpStatus.CREATED);
+        return new ResponseEntity<>(itemRepository.save(item), CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
+    @RequestMapping(method = PUT)
     @ApiOperation(value = "Update an Item", response = Item.class)
     public Item itemPut(@RequestBody Item item) {
         return itemRepository.save(item);
+    }
+
+    @RequestMapping(method = DELETE)
+    @ApiOperation(value = "Delete an Item")
+    public ResponseEntity itemDelete(@RequestBody Item item) {
+        itemRepository.deleteById(item.getId());
+        return new ResponseEntity(NO_CONTENT);
     }
 }
