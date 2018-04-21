6b9dd9aecd61353a3448c9618c29c0d5de79a72d
Marcel Gimeno Roman <marcel.gimeno@gmaill.com>
2018-04-21 19:56:23 +0200

    :sparkles: Ensure that data and functionality of implementations of Test classes is ok

--- src/test/java/com/mgr/life/IntegrationTest.java
+++ src/test/java/com/mgr/life/IntegrationTest.java
@@ -51,6 +51,7 @@ public abstract class IntegrationTest<T extends RestEntity> {
         Objects.requireNonNull(endPoint(), "API endpoint can't be bull.");
         Objects.requireNonNull(repository(), "API repository can't be null.");
         Objects.requireNonNull(newRestEntity(), "API newRestEntity can't be null.");
+        assertThat("RestEntity mustn't have id.", newRestEntity().getId(), nullValue());
         assertThat("RestEntity returned by modifyRestEntity method can't be equal to newRestEntity.",
                 newRestEntity(), not(equalTo(modifyRestEntity(newRestEntity()))));
 
